18e4324f5d375a67b07698897adc9501c55df225
yjchoigit <gmcadeau@gmail.com>
2024-06-25 16:12:44 +0900

    init commit

---
+++ src/main/java/com/hhplus/hhplus02/Hhplus02Application.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hhplus02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hhplus02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhplus/hhplus02/Hhplus02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.hhplus02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hhplus02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
