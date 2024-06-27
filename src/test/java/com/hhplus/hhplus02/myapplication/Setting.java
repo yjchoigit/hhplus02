e3e23778e8cec67f0ef99a7306996cf4b4616a5e
yjchoigit <yjchoi@gmail.com>
2024-06-27 16:41:03 +0900

    test: RestAssured 아직 쓰지 않는 기능 삭제

--- src/test/java/com/hhplus/hhplus02/myapplication/Setting.java
+++ src/test/java/com/hhplus/hhplus02/myapplication/Setting.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 
-import java.io.File;
 import java.util.Map;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,100 +36,12 @@ public class Setting {
                 .then().log().all().extract();
     }
 
-    public static ExtractableResponse<Response> get(String path, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .when().get(path)
-                .then().log().all().extract();
-    }
-
-    public static ExtractableResponse<Response> get(String path, Map<String, ?> parametersMap, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .queryParams(parametersMap)
-                .when().get(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> post(String path) {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post(path)
-                .then().log().all().extract();
-    }
-
     public static <T> ExtractableResponse<Response> post(String path, T requestBody) {
         return RestAssured
                 .given().log().all()
                 .body(requestBody)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().post(path)
-                .then().log().all().extract();
-    }
-
-    public static ExtractableResponse<Response> post(String path, File file, Map<String, ?> requestParam) {
-        return RestAssured
-                .given().log().all()
-                .multiPart("filesList", file)
-                .formParams(requestParam)
-                .when().post(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> post(String path, T requestBody, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().post(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> put(String path) {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> put(String path, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> put(String path, T requestBody) {
-        return RestAssured
-                .given().log().all()
-                .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> put(String path, T requestBody, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> patch(String path) {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().patch(path)
                 .then().log().all().extract();
     }
 
@@ -143,47 +54,4 @@ public class Setting {
                 .then().log().all().extract();
     }
 
-    public static <T> ExtractableResponse<Response> patch(String path, T requestBody, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().patch(path)
-                .then().log().all().extract();
-    }
-
-    public static ExtractableResponse<Response> delete(String path) {
-        return RestAssured
-                .given().log().all()
-                .when().delete(path)
-                .then().log().all().extract();
-    }
-
-    public static ExtractableResponse<Response> delete(String path, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .when().delete(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> delete(String path, T requestBody) {
-        return RestAssured
-                .given().log().all()
-                .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete(path)
-                .then().log().all().extract();
-    }
-
-    public static <T> ExtractableResponse<Response> delete(String path, T requestBody, String session) {
-        return RestAssured
-                .given().log().all()
-                .with().cookie("SESSION", session)
-                .body(requestBody)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete(path)
-                .then().log().all().extract();
-    }
 }
